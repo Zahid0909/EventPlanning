0f6117ccb9fd8050984cbb9d1cc68b0e5bac07d8
Zahid0909 <97629146+Zahid0909@users.noreply.github.com>
2023-06-06 17:16:16 +0300

    Creating Home Page for Event View
    Added with HTML grid to show 4 items in a row.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,11 +5,181 @@
         </h2>
     </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                Home Page
+    <div class="bg-indigo-50 px-4 py-8">
+        <div class="container mx-auto">
+
+            <div class="grid grid-cols-4 gap-6">
+
+                <div class="max-w-sm overflow-hidden rounded-xl bg-white shadow-md duration-200 hover:scale-105 hover:shadow-xl">
+                    <img src="https://th.bing.com/th/id/R.8b129b7ed53f4a9a109c78e78f6c1c3a?rik=kyqEQx0Ig612Ig&pid=ImgRaw&r=0" alt="plant" class="h-auto w-full" />
+                    <div class="p-5">
+                      <p class="text-medium mb-1 text-gray-700">
+                        Welcome to the TechConnect Summit, the premier gathering for technology enthusiasts, innovators, and industry leaders!
+                      </p>
+                      <p class="text-sm text-gray-500 mb-3">
+                        <span class="text-gray-500 mr-2">
+                          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="h-4 w-4 inline-block">
+                            <rect x="3" y="4" width="18" height="18" rx="2" ry="2" />
+                            <line x1="16" y1="2" x2="16" y2="6" />
+                            <line x1="8" y1="2" x2="8" y2="6" />
+                            <line x1="3" y1="10" x2="21" y2="10" />
+                          </svg>
+                        </span>
+                        Date: June 6, 2023
+                      </p>
+                      <p class="text-sm text-gray-500">
+                        <p class="text-sm text-gray-500 mb-3">
+                            <span class="text-gray-500 mr-2">
+                              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="feather feather-clock h-4 w-4 inline-block">
+                                <circle cx="12" cy="12" r="10"></circle>
+                                <polyline points="12 6 12 12 16 14"></polyline>
+                              </svg>
+                            </span>
+                            Time:  6:00pm - 10:00pm
+                          </p>
+                      <p class="text-sm text-gray-500">
+                        <span class="text-gray-500 mr-2">
+                          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="h-4 w-4 inline-block">
+                            <path d="M12 22s-8-4.5-8-11.8A8 8 0 0 1 12 2a8 8 0 0 1 8 8.2c0 7.3-8 11.8-8 11.8z" />
+                            <circle cx="12" cy="10" r="3" />
+                          </svg>
+                        </span>
+                        Location: Your Desired Location
+                      </p>
+                      <div class="mt-4">
+                        <button class="w-full rounded-md bg-indigo-600 py-2 text-indigo-100 hover:bg-indigo-500 hover:shadow-md duration-75">View</button>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div class="max-w-sm overflow-hidden rounded-xl bg-white shadow-md duration-200 hover:scale-105 hover:shadow-xl">
+                    <img src="https://th.bing.com/th/id/R.8b129b7ed53f4a9a109c78e78f6c1c3a?rik=kyqEQx0Ig612Ig&pid=ImgRaw&r=0" alt="plant" class="h-auto w-full" />
+                    <div class="p-5">
+                      <p class="text-medium mb-1 text-gray-700">
+                        Welcome to the TechConnect Summit, the premier gathering for technology enthusiasts, innovators, and industry leaders!
+                      </p>
+                      <p class="text-sm text-gray-500 mb-3">
+                        <span class="text-gray-500 mr-2">
+                          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="h-4 w-4 inline-block">
+                            <rect x="3" y="4" width="18" height="18" rx="2" ry="2" />
+                            <line x1="16" y1="2" x2="16" y2="6" />
+                            <line x1="8" y1="2" x2="8" y2="6" />
+                            <line x1="3" y1="10" x2="21" y2="10" />
+                          </svg>
+                        </span>
+                        Date: June 6, 2023
+                      </p>
+                      <p class="text-sm text-gray-500">
+                        <p class="text-sm text-gray-500 mb-3">
+                            <span class="text-gray-500 mr-2">
+                              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="feather feather-clock h-4 w-4 inline-block">
+                                <circle cx="12" cy="12" r="10"></circle>
+                                <polyline points="12 6 12 12 16 14"></polyline>
+                              </svg>
+                            </span>
+                            Time:  6:00pm - 10:00pm
+                          </p>
+                      <p class="text-sm text-gray-500">
+                        <span class="text-gray-500 mr-2">
+                          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="h-4 w-4 inline-block">
+                            <path d="M12 22s-8-4.5-8-11.8A8 8 0 0 1 12 2a8 8 0 0 1 8 8.2c0 7.3-8 11.8-8 11.8z" />
+                            <circle cx="12" cy="10" r="3" />
+                          </svg>
+                        </span>
+                        Location: Your Desired Location
+                      </p>
+                      <div class="mt-4">
+                        <button class="w-full rounded-md bg-indigo-600 py-2 text-indigo-100 hover:bg-indigo-500 hover:shadow-md duration-75">View</button>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div class="max-w-sm overflow-hidden rounded-xl bg-white shadow-md duration-200 hover:scale-105 hover:shadow-xl">
+                    <img src="https://th.bing.com/th/id/R.8b129b7ed53f4a9a109c78e78f6c1c3a?rik=kyqEQx0Ig612Ig&pid=ImgRaw&r=0" alt="plant" class="h-auto w-full" />
+                    <div class="p-5">
+                      <p class="text-medium mb-1 text-gray-700">
+                        Welcome to the TechConnect Summit, the premier gathering for technology enthusiasts, innovators, and industry leaders!
+                      </p>
+                      <p class="text-sm text-gray-500 mb-3">
+                        <span class="text-gray-500 mr-2">
+                          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="h-4 w-4 inline-block">
+                            <rect x="3" y="4" width="18" height="18" rx="2" ry="2" />
+                            <line x1="16" y1="2" x2="16" y2="6" />
+                            <line x1="8" y1="2" x2="8" y2="6" />
+                            <line x1="3" y1="10" x2="21" y2="10" />
+                          </svg>
+                        </span>
+                        Date: June 6, 2023
+                      </p>
+                      <p class="text-sm text-gray-500">
+                        <p class="text-sm text-gray-500 mb-3">
+                            <span class="text-gray-500 mr-2">
+                              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="feather feather-clock h-4 w-4 inline-block">
+                                <circle cx="12" cy="12" r="10"></circle>
+                                <polyline points="12 6 12 12 16 14"></polyline>
+                              </svg>
+                            </span>
+                            Time:  6:00pm - 10:00pm
+                          </p>
+                      <p class="text-sm text-gray-500">
+                        <span class="text-gray-500 mr-2">
+                          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="h-4 w-4 inline-block">
+                            <path d="M12 22s-8-4.5-8-11.8A8 8 0 0 1 12 2a8 8 0 0 1 8 8.2c0 7.3-8 11.8-8 11.8z" />
+                            <circle cx="12" cy="10" r="3" />
+                          </svg>
+                        </span>
+                        Location: Your Desired Location
+                      </p>
+                      <div class="mt-4">
+                        <button class="w-full rounded-md bg-indigo-600 py-2 text-indigo-100 hover:bg-indigo-500 hover:shadow-md duration-75">View</button>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div class="max-w-sm overflow-hidden rounded-xl bg-white shadow-md duration-200 hover:scale-105 hover:shadow-xl">
+                    <img src="https://th.bing.com/th/id/R.8b129b7ed53f4a9a109c78e78f6c1c3a?rik=kyqEQx0Ig612Ig&pid=ImgRaw&r=0" alt="plant" class="h-auto w-full" />
+                    <div class="p-5">
+                      <p class="text-medium mb-1 text-gray-700">
+                        Welcome to the TechConnect Summit, the premier gathering for technology enthusiasts, innovators, and industry leaders!
+                      </p>
+                      <p class="text-sm text-gray-500 mb-3">
+                        <span class="text-gray-500 mr-2">
+                          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="h-4 w-4 inline-block">
+                            <rect x="3" y="4" width="18" height="18" rx="2" ry="2" />
+                            <line x1="16" y1="2" x2="16" y2="6" />
+                            <line x1="8" y1="2" x2="8" y2="6" />
+                            <line x1="3" y1="10" x2="21" y2="10" />
+                          </svg>
+                        </span>
+                        Date: June 6, 2023
+                      </p>
+                      <p class="text-sm text-gray-500">
+                        <p class="text-sm text-gray-500 mb-3">
+                            <span class="text-gray-500 mr-2">
+                              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="feather feather-clock h-4 w-4 inline-block">
+                                <circle cx="12" cy="12" r="10"></circle>
+                                <polyline points="12 6 12 12 16 14"></polyline>
+                              </svg>
+                            </span>
+                            Time:  6:00pm - 10:00pm
+                          </p>
+                      <p class="text-sm text-gray-500">
+                        <span class="text-gray-500 mr-2">
+                          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="h-4 w-4 inline-block">
+                            <path d="M12 22s-8-4.5-8-11.8A8 8 0 0 1 12 2a8 8 0 0 1 8 8.2c0 7.3-8 11.8-8 11.8z" />
+                            <circle cx="12" cy="10" r="3" />
+                          </svg>
+                        </span>
+                        Location: Your Desired Location
+                      </p>
+                      <div class="mt-4">
+                        <button class="w-full rounded-md bg-indigo-600 py-2 text-indigo-100 hover:bg-indigo-500 hover:shadow-md duration-75">View</button>
+                      </div>
+                    </div>
+                  </div>
+
+
             </div>
         </div>
-    </div>
+      </div>
 </x-app-layout>
--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('home');
-});
-
 Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
@@ -26,3 +22,17 @@ Route::middleware([
         return view('dashboard');
     })->name('dashboard');
 });
+
+
+
+Route::get('event/{id}', function ($id) {
+    return view('event.show', [
+        'event' => $id
+    ]);
+})
+//->where('id', '[0-9]+')
+->name('event.show');
+
+Route::get('/', function () {
+    return view('home');
+})->name('home');
